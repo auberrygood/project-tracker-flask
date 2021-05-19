ed788f7375b41d839f1359ae9c56b22e6c2e1e3f
Vira Sukhanova <verusia.s@gmail.com>
2021-05-18 23:09:06 -0500

    Lab progress

--- hackbright_web.py
+++ hackbright_web.py
@@ -24,7 +24,31 @@ def get_student():
                            github=github)
     return html
     # return f"{github} is the GitHub account for {first} {last}"
+    
+@app.route("/student-search")
+def get_student_form():
+    """show form for searching for a student"""
 
+    return render_template("student_search.html")
+
+
+@app.route("/student-add", methods=['GET', 'POST'])
+def student_add():
+    """Add a new student"""
+    if request.method == 'POST':
+       fname =  request.form.get('fname')
+       lname =  request.form.get('lname')
+       github =  request.form.get('github')
+       
+    hackbright.make_new_student(fname, lname, github)  
+    
+    
+@app.route("/student-add-form", methods=['GET', 'POST'])
+def get_student_add_form():
+    """showing form to add student"""
+    return render_template('student_add.html')
+  
+    
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
